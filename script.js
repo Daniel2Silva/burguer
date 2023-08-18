e051c27314f96fc82ad6467fdd63ae8b0cd0b709
Daniel Silva <danicater@hotmail.com>
2023-08-18 16:37:11 -0300

    menu mobile aprimorado

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const menuMobile = document.querySelector('.js-mobile');
-function ativarMenu(){
-    const nav = document.querySelector('.nav-itens');
-    nav.classList.toggle('ativo');
-}
+function InitMenuMobile() {
+  const menuMobile = document.querySelector(".js-mobile");
+  function ativarMenu() {
+    const nav = document.querySelector(".nav-itens");
+    nav.classList.toggle("ativo");
+  }
 
-menuMobile.addEventListener('click', ativarMenu);
+  menuMobile.addEventListener("click", ativarMenu);
+}
+InitMenuMobile();
