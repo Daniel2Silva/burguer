5183c33bbf5d100f8454e4022ae39a2c2f0f8b7b
Daniel Silva <danicater@hotmail.com>
2023-08-18 05:56:34 -0300

    era pra ir todas as pastas...

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuMobile = document.querySelector('.js-mobile');
+function ativarMenu(){
+    const nav = document.querySelector('.nav-itens');
+    nav.classList.toggle('ativo');
+}
+
+menuMobile.addEventListener('click', ativarMenu);
